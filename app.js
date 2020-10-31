c9afba450e7c85a95c64b5a6600a8b64b28eaa5d
EvansVan <evansndiritu@gmail.com>
2020-11-01 01:49:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    .then((reg) => {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch((error) => {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+}
+var city = "";
+var url = '';
+const key = "5d94098c18ec2931455dcb1cb6771279";
+
+let weatherArray = JSON.parse(localStorage.getItem('weatherdata')) || [];
+
+document.addEventListener("DOMContentLoaded",function() {    
+  console.log("loaded");
+  getWeather(weatherArray);
+  console.table(weatherArray);
+});
+
+window.onload =function(){
+  submit = document.querySelector("#submit");
+  weather = document.querySelector("#weather");
+  
+  
+  submit.addEventListener("click",function(){
+    console.log("submit clicked");
+    city = document.getElementById("city").value;
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+    console.log(city);
+    $.getJSON(url, function (data) {
+      weatherArray.unshift(data);
+      localStorage.setItem('weatherdata', JSON.stringify(weatherArray));
+  });
+    getWeather(weatherArray);
+  });
+};  
+
+//function to display/populate the page
+const getWeather = (arr) => {
+  let mappedArr = (arr).map((data)=>{
+    return `<div class= "col-12 col-md-6 col-lg-4 py-2">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title" id="name">${data.name}</h5>
+                  <p class="card-text">Temp:<span id="temp">${data.main.temp}</p>
+                  <p class="card-text">Description:<span id="temp">${data.weather[0].description}</p>
+                  <p class="card-text">Temp:<span id="temp">${data.weather[0].icon}</p>
+                </div>
+              </div>
+            </div>`;
+  });
+  mappedArr = mappedArr.join("");
+  weather.innerHTML= mappedArr;
+};
+
+//function to get data from api
+function updateDOM(data){
+  
+}
---
+++ sw.js
@@ -0,0 +1,49 @@
+var cacheStore = "v1";
+
+var cacheAssets = [
+  "/index.html",
+  "/android-chrome-192x192.png",
+  "/app.js",
+  "/apple-touch-icon.png",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/favicon.ico",
+  "/icon.png"
+];
+
+// call install event to cache assets
+self.addEventListener('install',(e)=>{
+  console.log("Service worker installed");
+
+  e.waitUntil(
+    caches
+    .open(cacheStore)
+    .then((cache) => {
+      console.log("caching files");
+      cache.addAll(cacheAssets);
+    }) 
+  );
+});
+
+//call fetch event
+self.addEventListener('fetch', (e)=> {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
+
+//call the activate event
+self.addEventListener('activate',(e) =>{
+  console.log("worker activated");
+  e.waitUntil(
+    caches.keys().then((cacheNames)=>{
+      return Promise.all(
+        cacheNames.map((cache)=>{
+          if (cache !== cacheStore) {
+            console.log("clearing old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
