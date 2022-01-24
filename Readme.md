53e388da66b5f02d19e2ad370062fb42c85f43db
EvansVan <evansndiritu@gmail.com>
2022-01-25 01:00:44 +0300

    second commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Weather App
+Progressive webapp using the Open weather api ('https://api.openweathermap.org/) to render weather data for a specific location.
+
+# General Info
+This project is a PWA that  uses modern web capabilities to deliver an app-like user experience .Leveraging the use of a service worker, local storage and manifest to enable user to install on their respective devices.
--- sw.js
+++ sw.js
@@ -1,49 +1,47 @@
-var cacheStore = "v1";
+var cacheStore = 'v1'
 
 var cacheAssets = [
-  "/index.html",
-  "/android-chrome-192x192.png",
-  "/app.js",
-  "/apple-touch-icon.png",
-  "/browserconfig.xml",
-  "/favicon-16x16.png",
-  "/favicon-32x32.png",
-  "/favicon.ico",
-  "/icon.png"
-];
+  '/index.html',
+  '/android-chrome-192x192.png',
+  '/app.js',
+  '/apple-touch-icon.png',
+  '/browserconfig.xml',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/favicon.ico',
+  '/icon.png'
+]
 
 // call install event to cache assets
-self.addEventListener('install',(e)=>{
-  console.log("Service worker installed");
+self.addEventListener('install', (e) => {
+  console.log('Service worker installed')
 
   e.waitUntil(
-    caches
-    .open(cacheStore)
-    .then((cache) => {
-      console.log("caching files");
-      cache.addAll(cacheAssets);
-    }) 
-  );
-});
+    caches.open(cacheStore).then((cache) => {
+      console.log('caching files')
+      cache.addAll(cacheAssets)
+    })
+  )
+})
 
 //call fetch event
-self.addEventListener('fetch', (e)=> {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+})
 
 //call the activate event
-self.addEventListener('activate',(e) =>{
-  console.log("worker activated");
+self.addEventListener('activate', (e) => {
+  console.log('worker activated')
   e.waitUntil(
-    caches.keys().then((cacheNames)=>{
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cache)=>{
+        cacheNames.map((cache) => {
           if (cache !== cacheStore) {
-            console.log("clearing old cache");
-            return caches.delete(cache);
+            console.log('clearing old cache')
+            return caches.delete(cache)
           }
         })
-      );
+      )
     })
-  );
-});
+  )
+})
