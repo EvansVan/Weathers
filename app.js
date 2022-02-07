9d8951a066622888f0c0ecbf25598e6a893a369e
EvansVan <evansndiritu@gmail.com>
2022-02-07 14:32:11 +0300

    third commit

--- app.js
+++ app.js
@@ -14,36 +14,15 @@ let city = ''
 let url = ''
 const key = '5d94098c18ec2931455dcb1cb6771279'
 
+// get data from local storage and call function to render the page if there is data
 const weatherArray = JSON.parse(localStorage.getItem('weatherdata')) || []
-
 document.addEventListener('DOMContentLoaded', function () {
   console.log('loaded')
   getWeather(weatherArray)
   console.table(weatherArray)
 })
 
-window.onload = function () {
-  submit = document.querySelector('#submit')
-  weather = document.querySelector('#weather')
-
-  submit.addEventListener('click', function () {
-    console.log('submit clicked')
-    city = document.getElementById('city').value
-    url =
-      'https://api.openweathermap.org/data/2.5/weather?q=' +
-      city +
-      '&appid=' +
-      key
-    console.log(city)
-    $.getJSON(url, function (data) {
-      weatherArray.unshift(data)
-      localStorage.setItem('weatherdata', JSON.stringify(weatherArray))
-    })
-    getWeather(weatherArray)
-  })
-}
-
-// function to display/populate the page
+// function to display/render data to the page
 const getWeather = (arr) => {
   let mappedArr = arr.map((data) => {
     return `<div class= "col-12 col-md-6 col-lg-4 py-2">
@@ -59,4 +38,21 @@ const getWeather = (arr) => {
   })
   mappedArr = mappedArr.join('')
   weather.innerHTML = mappedArr
+}
+
+window.onload = function () {
+  submit = document.querySelector('#submit')
+  weather = document.querySelector('#weather')
+
+  submit.addEventListener('click', function () {
+    console.log('submit clicked')
+    city = document.getElementById('city').value
+    url ='https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key
+    console.log(city)
+    $.getJSON(url, function (data) {
+      weatherArray.unshift(data)
+      localStorage.setItem('weatherdata', JSON.stringify(weatherArray))
+      getWeather(weatherArray)
+    })
+  })
 }
